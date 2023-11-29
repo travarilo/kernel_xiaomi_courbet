RyuujiX/android_kernel_asus_sdm660

fs: Don't run ksu command when KernelSU is disabled
* For this commit: https://github.com/RyuujiX/KernelSU/commit/2d9aebf64b30946ecb98c81bba1ebffdd64a8f2f

Signed-off-by: RyuujiX <saputradenny712@gmail.com>

fs/stat.c
@@ -15,6 +15,10 @@
 #include <linux/syscalls.h>
 #include <linux/pagemap.h>
 
+#ifdef CONFIG_KSU
+#include <linux/ksu.h>
+#endif
+
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
 
@@ -99,7 +103,8 @@ int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 	unsigned int lookup_flags = 0;
 	
 #ifdef CONFIG_KSU
-	ksu_handle_stat(&dfd, &filename, &flag);
+	if (get_ksu_state() > 0)
+		ksu_handle_stat(&dfd, &filename, &flag);
 #endif
 
 	if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |

travarilo/kernel_xiaomi_courbet

Change-Id: I741e1785e35dccda3acc600d4a2fa25485d7c6a8
Signed-off-by: TogoFire <togofire@mailfence.com>

fs/stat.c
@@ -19,6 +19,11 @@
 #include <linux/compat.h>
 
 #include <linux/uaccess.h>
+
+#ifdef CONFIG_KSU
+#include <linux/ksu.h>
+#endif
+
 #include <asm/unistd.h>
 
 /**
@@ -175,7 +180,8 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
 
 #ifdef CONFIG_KSU
-	ksu_handle_stat(&dfd, &filename, &flags);
+	if (get_ksu_state() > 0)
+		ksu_handle_stat(&dfd, &filename, &flags);
 #endif
 
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |